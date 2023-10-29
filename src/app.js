c11eaab473e632f776e78b3fa0561b58bcf05d23
mtw-programmer <visnia58@gmail.com>
2023-10-29 20:24:25 +0100

    feat: add renderTasks function and await to calls

--- src/app.js
+++ src/app.js
@@ -1,16 +1,15 @@
 App = {
   loading: false,
   contracts: {},
-  load: () => {
-    App.loadAccount();
-    App.loadContract();
-    App.render();
+  load: async () => {
+    await App.loadAccount();
+    await App.loadContract();
+    await App.render();
   },
   loadAccount: async () => {
     try {
       const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
       App.account = accounts[0];
-      $('#account').html(App.account);
     } catch (ex) {
       console.log(ex);
     }
@@ -21,10 +20,44 @@ App = {
     App.contracts.toDoList.setProvider(ethereum);
     App.toDoList = await App.contracts.toDoList.deployed();
   },
-  render: () => {
+  render: async () => {
     if (App.loading) return;
 
+    App.setLoading(true);
+
+    $('#account').html(App.account);
+
+    await App.renderTasks();
+
     App.setLoading(false);
+  },
+  renderTasks: async () => {
+    const taskCount = await App.toDoList.taskCount();
+    const taskTemplate = $('.taskTemplate');
+
+    for (let i = 1; i <= taskCount; i++) {
+      const task = await App.toDoList.tasks(i);
+      const id = task[0].toNumber();
+      const title = task[1];
+      const content = task[2];
+      const completed = task[3];
+
+      const newTaskTemplate = taskTemplate.clone();
+      
+      newTaskTemplate.find('.title').html(title);
+      newTaskTemplate.find('.content').html(content);
+      newTaskTemplate.find('input')
+        .prop('name', id)
+        .prop('checked', completed)
+        .on('click', App.toggleCompleted);
+
+      if (completed)
+        $('#completedTaskList').append(newTaskTemplate)
+      else
+        $('#taskList').append(newTaskTemplate);
+
+      newTaskTemplate.show();
+    }
   },
   setLoading: (bool) => {
     App.loading = bool;
