2a9863f3bf422b4d1fa50507c4c5a8dd3d95660b
mtw-programmer <visnia58@gmail.com>
2023-10-29 16:00:09 +0100

    feat: add Task struct and define tasks

--- contracts/ToDoList.sol
+++ contracts/ToDoList.sol
@@ -2,4 +2,13 @@ pragma solidity ^0.5.0;
 
 contract ToDoList {
   uint public taskCount = 0;
+
+  struct Task {
+    uint id;
+    string title;
+    string content;
+    bool completed = false;
+  }
+
+  mapping(uint => Task) public tasks;
 }
